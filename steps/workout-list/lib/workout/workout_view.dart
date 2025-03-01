0683df5b2291f78c4353a2e368d2641e6855c877
Tadas Petra <tadas@tadaspetra.com>
2025-03-01 15:24:21 -0600

    display workout information

---
+++ steps/workout-list/lib/workout/workout_view.dart
@@ -0,0 +1,85 @@
+import 'package:demo/auth/user_service.dart';
+import 'package:demo/core/locator.dart';
+import 'package:demo/dataviewer/data_view.dart';
+import 'package:demo/workout/workout_repository.dart';
+import 'package:demo/workout/workout_view_model.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutView extends StatefulWidget {
+  const WorkoutView({super.key});
+
+  @override
+  State<WorkoutView> createState() => _WorkoutViewState();
+}
+
+class _WorkoutViewState extends State<WorkoutView> {
+  late final WorkoutViewModel homeViewModel = WorkoutViewModel(
+    userService: locator<UserService>(),
+    workoutRepository: locator<WorkoutRepository>(),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Workout Tracker'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DataView(),
+                ),
+              );
+            },
+            child: const Text('Show Database'),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ...homeViewModel.exerciseSets.entries.map((entry) => ExerciseCard(
+                  name: entry.key,
+                  exercise: entry.value,
+                )),
+            TextButton(
+              onPressed: () {
+                homeViewModel.logout();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Logged out'),
+                  ),
+                );
+              },
+              child: Text('Logout'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExerciseCard extends StatelessWidget {
+  const ExerciseCard({
+    super.key,
+    required this.exercise,
+    required this.name,
+  });
+
+  final ValueNotifier<List<int>> exercise;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: exercise,
+      builder: (context, value, child) {
+        return Text('$name: $value');
+      },
+    );
+  }
+}
