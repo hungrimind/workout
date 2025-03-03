2715bed52c4aad9a4e6e3a6c1efd76767d3c5ce9
Tadas Petra <tadas@tadaspetra.com>
2025-03-03 12:12:49 -0600

    add initial add remove sets

---
+++ _initial/add-remove-sets/lib/workout/workout_view.dart
@@ -0,0 +1,66 @@
+import 'package:demo/auth/user_service.dart';
+import 'package:demo/core/locator.dart';
+import 'package:demo/dataviewer/data_view.dart';
+import 'package:demo/workout/workout_view_model.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutView extends StatefulWidget {
+  const WorkoutView({super.key});
+
+  @override
+  State<WorkoutView> createState() => _WorkoutViewState();
+}
+
+class _WorkoutViewState extends State<WorkoutView> {
+  late final WorkoutViewModel workoutViewModel = WorkoutViewModel(
+    userService: locator<UserService>(),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Workout Tracker'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DataView(),
+                ),
+              );
+            },
+            child: const Text('Show Database'),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: workoutViewModel.userNotifier,
+              builder: (context, user, child) {
+                return Text('Welcome ${user?.name}',
+                    style: Theme.of(context).textTheme.headlineLarge);
+              },
+            ),
+            TextButton(
+              onPressed: () {
+                workoutViewModel.logout();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Logged out'),
+                  ),
+                );
+              },
+              child: Text('Logout'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ _initial/add-remove-sets/lib/workout/workout_view_model.dart
@@ -0,0 +1,36 @@
+import 'package:demo/auth/user.dart';
+import 'package:demo/auth/user_service.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutViewModel {
+  WorkoutViewModel({required UserService userService})
+      : _userService = userService {
+    _initExercises();
+  }
+
+  final UserService _userService;
+  ValueNotifier<User?> get userNotifier => _userService.userNotifier;
+
+  final exercises = ['Push-ups', 'Pull-ups', 'Sit-ups', 'Squats'];
+  final Map<String, ValueNotifier<List<int>>> exerciseSets = {};
+
+  void _initExercises() {
+    for (var exercise in exercises) {
+      exerciseSets[exercise] = ValueNotifier([]);
+    }
+  }
+
+  void addSet(String exerciseName, int reps) {
+    final currentSets = List<int>.from(exerciseSets[exerciseName]!.value);
+    exerciseSets[exerciseName]!.value = currentSets;
+  }
+
+  void removeSet(String exercise, int index) {
+    final currentSets = List<int>.from(exerciseSets[exercise]!.value);
+    exerciseSets[exercise]!.value = currentSets;
+  }
+
+  void logout() {
+    _userService.signOut();
+  }
+}
